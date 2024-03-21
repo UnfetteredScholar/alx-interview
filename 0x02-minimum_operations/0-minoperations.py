53be131066a9b944bec953a6e5372cc339eb8a07
Ato Toffah <atotoffah@gmail.com>
2024-03-21 17:07:56 +0000

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Defines the function minOperations
+"""
+
+
+def isPrime(num: int) -> bool:
+    """Checks in a number is prime"""
+    i = 2
+    mid = num / 2
+    while i < mid:
+        if num % i == 0:
+            return False
+        i += 1
+    return True
+
+
+def minOperations(n: int) -> int:
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    Operations: CopyAll and Paste
+
+    Args:
+        n: target number of characters
+
+    Returns:
+        The number of operations
+    """
+    count = 1
+    clipboard = 0
+    operations = 0
+
+    while count < n:
+        if (n % count == 0) and isPrime(count):
+            clipboard = count
+            operations += 1
+        count += clipboard
+        operations += 1
+    return operations
